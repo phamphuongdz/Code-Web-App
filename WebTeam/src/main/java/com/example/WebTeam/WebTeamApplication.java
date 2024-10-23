9171748b57336c0f5371ae9a12e66b73212ea4e9
Minh Duc <ductmth2306007@fpt.edu.vn>
2024-10-23 11:18:29 +0700

    Spring Boot Setup

---
+++ WebTeam/src/main/java/com/example/WebTeam/WebTeamApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebTeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTeamApplication.class, args);
+	}
+
+}
---
+++ WebTeam/src/test/java/com/example/WebTeam/WebTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebTeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
